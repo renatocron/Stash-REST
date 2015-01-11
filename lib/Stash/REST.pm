8c4628951bd837e3afc75e40baea4ad0a12e20e8
Renato CRON <renato.cron@gmail.com>
2015-01-11 04:53:40 -0200

    initial commit

---
+++ lib/Stash/REST.pm
@@ -0,0 +1,39 @@
+package Stash::REST;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Stash::REST - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Stash::REST;
+
+=head1 DESCRIPTION
+
+Stash::REST is
+
+=head1 AUTHOR
+
+Renato CRON E<lt>rentocron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Renato CRON
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+use Stash::REST;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
