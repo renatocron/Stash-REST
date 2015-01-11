c6434e12b15fee642ccaa4657f3da49ed050049a
Renato CRON <renato.cron@gmail.com>
2015-01-11 04:57:48 -0200

    first lib/Stash/REST.pm real version

--- lib/Stash/REST.pm
+++ lib/Stash/REST.pm
@@ -1,10 +1,312 @@
 package Stash::REST;
-
 use strict;
 use 5.008_005;
 our $VERSION = '0.01';
 
+use Moo;
+use warnings;
+use utf8;
+use URI;
+use JSON;
+use HTTP::Request::Common qw(GET POST DELETE HEAD);
+use Carp;
+
+has 'do_request' => (
+    is => 'rw',
+    isa => sub {die "$_[0] is not a CodeRef" unless ref $_[0] eq 'CODE'},
+    required => 1
+);
+has 'stash' => (
+    is => 'rw',
+    isa => sub {die "$_[0] is not a HashRef" unless ref $_[0] eq 'HASH'},
+    default => sub { {} }
+);
+
+has 'fixed_headers' => (
+    is => 'rw',
+    isa => sub {die "$_[0] is not a ArrayRef" unless ref $_[0] eq 'ARRAY'},
+    default => sub { [] }
+);
+
+around 'stash' => sub {
+    my $orig  = shift;
+    my $c     = shift;
+    my $stash = $orig->($c);
+
+    if (@_) {
+        return $stash->{ $_[0] } if ( @_ == 1 && ref $_[0] eq '' );
+
+        my $new_stash = @_ > 1 ? {@_} : $_[0];
+        die('stash takes a hash or hashref') unless ref $new_stash;
+        foreach my $key ( keys %$new_stash ) {
+            $stash->{$key} = $new_stash->{$key};
+        }
+    }
+
+    return $stash;
+};
+
+sub rest_put {
+    my $self = shift;
+    my $url  = shift;
+    my $data = pop || [];
+    my %conf = @_;
+
+    $self->rest_post(
+        $url,
+        code => ( exists $conf{is_fail} ? 400 : 202 ),
+        %conf,
+        method => 'PUT',
+        $data
+    );
+}
+
+sub rest_head {
+    my $self = shift;
+    my $url  = shift;
+    my $data = pop || [];
+    my %conf = @_;
+
+    $self->rest_post(
+        $url,
+        code => 200,
+        %conf,
+        method => 'HEAD',
+        $data
+    );
+}
+
+sub rest_delete {
+    my $self = shift;
+    my $url  = shift;
+    my $data = pop || [];
+    my %conf = @_;
+
+    $self->rest_post(
+        $url,
+        code => 204,
+        %conf,
+        method => 'DELETE',
+        $data
+    );
+}
+
+sub rest_get {
+    my $self = shift;
+    my $url  = shift;
+    my $data = pop || [];
+    my %conf = @_;
+
+    $self->rest_post(
+        $url,
+        code => 200,
+        %conf,
+        method => 'GET',
+        $data
+    );
+}
+
+sub rest_post {
+    my $self = shift;
+    my $url  = shift;
+    my $data = pop || [];
+    my %conf = @_;
+
+    $url = join '/', @$url if ref $url eq 'ARRAY';
+
+    my $is_fail = exists $conf{is_fail} && $conf{is_fail};
+
+    my $code = $conf{code};
+    $code ||= $is_fail ? 400 : 201;
+
+
+
+    my $stashkey = exists $conf{stash} ? $conf{stash} : undef;
+
+    my @headers = (@{$self->fixed_headers()}, @{$conf{headers}||[]} );
+
+    my $req;
+
+    if ( !exists $conf{files} ) {
+        $req = POST $url, $data, @headers;
+    }
+    else {
+        $conf{files}{$_} = [ $conf{files}{$_} ] for keys %{ $conf{files} };
+
+        $req = POST $url,
+          @headers,
+          'Content-Type' => 'form-data',
+          Content => [ @$data, %{ $conf{files} } ];
+    }
+
+    $req->method( $conf{method} ) if exists $conf{method};
+
+    my $res = eval{$self->do_request()->($req)};
+    croak "request died: $@" if $@;
+
+    #is( $res->code, $code, $name . ' status code is ' . $code );
+    croak 'request success diverge expected' if ($is_fail && $res->is_success) || (!$res->is_success);
+    croak 'request code diverge expected' if $code != $res->code;
+
+    return '' if $code == 204;
+
+    my $obj = eval { decode_json( $res->content ) };
+    #fail($@) if $@;
+
+    if ($stashkey) {
+        $self->stash->{$stashkey} = $obj;
+
+        $self->stash( $stashkey . '.prepare_request' => $conf{prepare_request} ) if exists $conf{prepare_request};
+
+        if ( $code == 201 ) {
+            $self->stash( $stashkey . '.id' => $obj->{id} );
+
+            my $item_url = $res->header('Location');
+
+            if ($item_url){
+                $self->stash->{$stashkey . '.url'} = $item_url ;
+
+                $self->rest_reload($stashkey);
+            }else{
+                croak 'requests with response code 201 should contain header Location';
+            }
+        }
+    }
+
+    if ( $stashkey && exists $conf{list} ) {
+
+        $self->stash( $stashkey . '.list-url' => $url );
+
+        $self->rest_reload_list($stashkey);
+
+    }
+
+    return $obj;
+}
+
+
+sub rest_reload {
+    my $self     = shift;
+    my $stashkey = shift;
+
+    my %conf = @_;
+
+    my $code = exists $conf{code} ? $conf{code} : 200;
+
+
+    my @headers = (@{$self->fixed_headers()}, @{$conf{headers}||[]} );
+    my $item_url = $self->stash->{ $stashkey . '.url' };
+
+    croak "can't stash $stashkey.url is not valid" unless $item_url;
+
+    my $prepare_request =
+      exists $self->stash->{ $stashkey . '.prepare_request' }
+      && ref $self->stash->{ $stashkey . '.prepare_request' } eq 'CODE'
+      ? $self->stash->{ $stashkey . '.prepare_request' }
+      : undef;
+
+    my $req = POST $item_url, [];
+    $req->method('GET');
+    $prepare_request->($req) if $prepare_request;
+
+    my $res = $self->do_request()->($req);
+
+    croak 'request code diverge expected' if $code != $res->code;
+
+    my $obj;
+    if ( $res->code == 200 ) {
+        $obj = eval { decode_json( $res->content ) };
+        if ($@) {
+            fail($@);
+            eval('use Data::Printer; p $res');
+        }
+
+        $self->stash( $stashkey . '.get' => $obj );
+    }
+    elsif ( $res->code == 404 ) {
+
+        #ok( !$res->is_success, 'GET ' . $item_url . ' does not exists' );
+        #is( $res->code, 404, 'GET ' . $item_url . ' status code is 404' );
+
+        delete $self->stash->{ $stashkey . '.get' };
+        delete $self->stash->{ $stashkey . '.id' };
+        delete $self->stash->{ $stashkey . '.url' };
+        delete $self->stash->{ $stashkey };
+
+    }
+    else {
+        croak 'response code ' . $res->code . ' is not valid for rest_reload';
+    }
+
+    return $obj;
+}
+
+
+sub rest_reload_list {
+    my $self     = shift;
+    my $stashkey = shift;
+
+    my %conf = @_;
+
+    my $code = exists $conf{code} ? $conf{code} : 200;
+
+    my @headers = (@{$self->fixed_headers()}, @{$conf{headers}||[]} );
+    my $item_url = $self->stash->{ $stashkey . '.list-url' };
+
+    croak "can't stash $stashkey.list-url is not valid" unless $item_url;
+
+    my $prepare_request =
+      exists $self->stash->{ $stashkey . '.prepare_request' }
+      && ref $self->stash->{ $stashkey . '.prepare_request' } eq 'CODE'
+      ? $self->stash->{ $stashkey . '.prepare_request' }
+      : undef;
+
+    my $req = POST $item_url, [];
+    $req->method('GET');
+    $prepare_request->($req) if $prepare_request;
+
+
+    my $res = $self->do_request()->($req);
+
+    croak 'request code diverge expected' if $code != $res->code;
+
+    my $obj;
+    if ( $res->code == 200 ) {
+        #if ( !ok( $res->is_success, 'GET ' . $item_url . ' is_success' ) ) {
+        #    eval('use Data::Printer; p $res');
+        #}
+
+        $obj = eval { decode_json( $res->content ) };
+        if ($@) {
+            fail($@);
+            eval('use Data::Printer; p $res');
+        }
+
+        $self->stash( $stashkey . '.list' => $obj );
+    }
+    elsif ( $res->code == 404 ) {
+
+        delete $self->stash->{ $stashkey . '.list' };
+        delete $self->stash->{ $stashkey . '.list-url' };
+        delete $self->stash->{ $stashkey };
+
+    }
+    else {
+        croak 'response code ' . $res->code . ' is not valid for rest_reload';
+    }
+
+    return $obj;
+}
+
+sub stash_ctx {
+    my ( $self, $staname, $sub ) = @_;
+
+    $sub->( $self->stash->{$staname} );
+}
+
+
 1;
+
 __END__
 
 =encoding utf-8
@@ -20,6 +322,23 @@ Stash::REST - Blah blah blah
 =head1 DESCRIPTION
 
 Stash::REST is
+
+
+=head1 METHODS
+
+
+=head2 $t->stash
+
+Copy from old Catalyst.pm, but $t->stash('foo') = $t->stash->{'foo'}
+
+Returns a hashref to the stash, which may be used to store data and pass
+it between components during a test. You can also set hash keys by
+passing arguments. Unlike catalyst, it's never cleared, so, it lasts until object this destroy.
+
+    $t->stash->{foo} = $bar;
+    $t->stash( { moose => 'majestic', qux => 0 } );
+    $t->stash( bar => 1, gorch => 2 ); # equivalent to passing a hashref
+
 
 =head1 AUTHOR
 
--- t/basic.t
+++ t/basic.t
@@ -1,8 +1,183 @@
 use strict;
 use Test::More;
-use Stash::REST;
 
-# replace with the actual test
-ok 1;
+use JSON;
+use HTTP::Response;
+use Test::Fake::HTTPD;
+use LWP::UserAgent;
+use URL::Encode qw/url_params_mixed/;
+my $httpd = Test::Fake::HTTPD->new( timeout => 5, );
+
+my $fakedatabase = {};
+my $seq          = 0;
+$httpd->run(
+    sub {
+        my $req = shift;
+
+        my @pp = split q{/}, $req->uri->path;
+        shift @pp;
+        $seq++;
+
+        my $data = $req->content;
+        $data = url_params_mixed($data) if $data;
+
+        if ( $req->method eq 'POST' ) {
+
+            if ( @pp == 1 ) {
+
+                $fakedatabase->{ $pp[0] }{$seq} = my $obj = { id => $seq, %$data };
+                return [
+                    201,
+                    [ 'Content-Type', 'application/json', 'Location', join( '/', @pp, $seq ) ],
+                    [ encode_json($obj) ]
+                ];
+            }
+
+        }
+        elsif ( $req->method eq 'GET' ) {
+
+            if ( @pp == 1 ) {
+                return [
+                    200,
+                    [ 'Content-Type', 'application/json' ],
+                    [
+                        encode_json(
+                            { $pp[0], [ map { $fakedatabase->{ $pp[0] }{$_} } keys %{ $fakedatabase->{ $pp[0] } } ] }
+                        )
+                    ]
+                ];
+            }
+            else {
+                return [ 404, [], [] ] unless exists $fakedatabase->{ $pp[0] }{ $pp[1] };
+                return [
+                    200,
+                    [ 'Content-Type', 'application/json' ],
+                    [ encode_json( $fakedatabase->{ $pp[0] }{ $pp[1] } ) ]
+                ];
+            }
+
+        }
+        elsif ( $req->method eq 'DELETE' ) {
+
+            if ( @pp == 2 ) {
+                delete $fakedatabase->{ $pp[0] }{ $pp[1] };
+                return [ 204, [], [''] ];
+            }
+
+        }
+        elsif ( $req->method eq 'PUT' ) {
+
+            if ( @pp == 2 ) {
+
+                $fakedatabase->{ $pp[0] }{ $pp[1] }{$_} = $data->{$_} for keys %$data;
+                return [
+                    202,
+                    [ 'Content-Type', 'application/json' ],
+                    [ encode_json( { id => $fakedatabase->{ $pp[0] }{ $pp[1] }{id} } ) ]
+                ];
+            }
+
+        }
+
+        [ 500, [ 'Content-Type', 'application/json' ], ['{"error":"fatal"}'] ];
+    }
+);
+
+BEGIN { use_ok 'Stash::REST' }
+my $obj = Stash::REST->new(
+    do_request => sub {
+        my $req = shift;
+        $req->uri( $req->uri->abs( $httpd->endpoint ) );
+
+        LWP::UserAgent->new->request($req);
+    },
+);
+is( ref $obj, 'Stash::REST', 'obj is Stash::REST' );
+
+is( $obj->stash('foo'), undef, 'foo is undef' );
+
+is( $obj->stash->{'foo'} = 3, '3', 'foo is 3' );
+
+$obj->stash->{'bar'} = 1;
+
+$obj->stash( 'bar2' => 2, 'bar3' => 3 );
+
+is( $obj->stash->{'bar'},  '1', 'bar is 1' );
+is( $obj->stash->{'bar2'}, '2', 'bar2 is 2' );
+is( $obj->stash->{'bar3'}, '3', 'bar3 is 3' );
+
+$obj->rest_post(
+    '/zuzus',
+    name  => 'add zuzu',
+    list  => 1,
+    stash => 'easyname',
+    [ name => 'foo', ]
+);
+
+is( ref $obj->stash->{'easyname'},      'HASH',    'stash easyname is hash' );
+is( ref $obj->stash->{'easyname.get'},  'HASH',    'stash easyname.get is hash' );
+is( ref $obj->stash->{'easyname.id'},   '',        'stash easyname.id is scalar' );
+is( $obj->stash->{'easyname.url'},      'zuzus/1', 'stash easyname.url is /zuzus/1' );
+is( $obj->stash->{'easyname.list-url'}, '/zuzus',  'stash easyname.list-url is /zuzus' );
+is( ref $obj->stash->{'easyname.list'}, 'HASH',    'stash easyname.list is HASH' );
+
+$obj->stash_ctx(
+    'easyname.get',
+    sub {
+        my ($me) = @_;
+
+        is( $me->{id}, $obj->stash('easyname.id'), 'get has the same id!' );
+        is( $me->{name}, 'foo', 'name ok!' );
+
+    }
+);
+
+$obj->stash_ctx(
+    'easyname.list',
+    sub {
+        my ($me) = @_;
+
+        ok( $me = delete $me->{zuzus}, 'zuzu list exists' );
+
+        is( @$me, 1, '1 zuzu' );
+
+        is( $me->[0]{name}, 'foo', 'listing ok' );
+    }
+);
+
+$obj->rest_put(
+    $obj->stash('easyname.url'),
+    name => 'update zuzu',
+    [ name => 'AAAAAAAAA', ]
+);
+
+$obj->rest_reload('easyname');
+
+$obj->stash_ctx(
+    'easyname.get',
+    sub {
+        my ($me) = @_;
+
+        is( $me->{name}, 'AAAAAAAAA', 'name updated!' );
+    }
+);
+
+$obj->rest_delete( $obj->stash('easyname.url') );
+
+$obj->rest_reload( 'easyname', code => 404 );
+
+$obj->rest_reload_list('easyname');
+
+$obj->stash_ctx(
+    'easyname.list',
+    sub {
+        my ($me) = @_;
+
+        ok( $me = delete $me->{zuzus}, 'zuzus list exists' );
+
+        is( @$me, 0, '0 zuzu' );
+
+    }
+);
 
 done_testing;
